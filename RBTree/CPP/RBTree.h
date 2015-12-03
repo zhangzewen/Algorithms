6648267e89ae71cb6658507538f691b3a04eb937
zhangzewen <daixiuhua_1989@163.com>
2015-12-03 10:31:52 -0500

    modify

--- RBTree/CPP/RBTree.h
+++ RBTree/CPP/RBTree.h
@@ -1,30 +1,6 @@
 #ifndef _RBTREE_H_INCLUDED__
 #define _RBTREE_H_INCLUDED__
 
-typedef int key_t;
-typedef int data_t;
-
-typedef enum color_t{
-	RED = 0,
-	BLACK = 1
-}color_t;
-
-typedef struct rb_node_t{
-	struct rb_node_t *left, *right, *parent;
-	key_t key;
-	data_t data;
-	color_t color;
-	int lflag;
-	int rflag;
-}rb_node_t;
-
-rb_node_t *rb_insert(key_t key, data_t data, rb_node_t *root);
-rb_node_t *rb_search(key_t key, rb_node_t *root);
-rb_node_t *rb_erase(key_t key, rb_node_t *root);
-void rb_dump(rb_node_t *root);
-void rb_free(rb_node_t **root);
-
-
 class RBTree
 {
 public:
@@ -40,16 +16,17 @@ public:
     COLOR color;
   }Node;
 
-  RBTree() : root(NULL) {
+  RBTree() : root(0) {
   }
   bool insert(int key, int value);
+  bool insert(Node** root, Node* nodeShouldInsert);
   bool search(int key);
   bool remove(int key);
   bool leftRotate(Node** root, Node* NodeShouldRotated);
   bool rightRotate(Node** root, Node* NodeShouldRotated);
   bool insertFixup(Node** root, Node* NodeshouldFixup);
-  Node* minimun(Node* node);
-  Node* Maxinum(Node* node);
+  Node* minimum(Node* node);
+  Node* maximum(Node* node);
   Node* successor(Node* node);
   Node* perdecessor(Node* node);
 private:
